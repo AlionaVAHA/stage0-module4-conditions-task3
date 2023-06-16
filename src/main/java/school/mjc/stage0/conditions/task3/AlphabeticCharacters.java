9c096e94f5d162045c131e691f37b55fbba5d72d
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 22:26:20 -0400

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        int symbolInt = symbol;
+        if (symbolInt==97 || symbolInt == 101 || symbolInt == 105 ||
+        symbolInt == 111 || symbolInt ==117){
+            System.out.println("Vowel");
+        } else if ((symbolInt >= 97 && symbolInt <= 122) || (symbolInt >= 41 && symbolInt <= 90)) {
+            System.out.println("Consonant");
+        }
+        else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
