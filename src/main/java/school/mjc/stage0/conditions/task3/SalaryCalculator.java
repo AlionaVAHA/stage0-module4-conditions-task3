05ae723c4f1f793850478c8b6b2fdb343cb68703
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 22:02:41 -0400

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+          if (salary<=10000){
+           float taxes = (float) (salary*0.15);
+           float result = salary-taxes;
+           System.out.println(result);
+       } else if (salary > 10000&&salary<=20000) {
+           float taxes = (float) (salary*0.18);
+           float result = salary-taxes;
+           System.out.println(result);
+       } else if (salary>20000) {
+           float taxes = (float) (salary*0.20);
+           float result = salary-taxes;
+           System.out.println(result);
+       }
+       else{
+           System.out.println("wrong input!");
+       }
     }
 }
