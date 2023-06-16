33f6f488f5768006d53ec0634823bbc66900ed93
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 21:53:41 -0400

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if ((symbolInt >= 97 && symbolInt <= 122)) {
+            System.out.println("English");
+        } else if (symbolInt >= 41 && symbolInt <= 90) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
